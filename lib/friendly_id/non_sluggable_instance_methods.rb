69f38e251bb8d6fe0d1e77b2aed25b02dd7d6af7
AmiableCoder <ian@amiablecoder.com>
2010-01-19 14:13:14 -0500

    Added a check for those models not using friendly_id

--- lib/friendly_id/non_sluggable_instance_methods.rb
+++ lib/friendly_id/non_sluggable_instance_methods.rb
@@ -56,7 +56,7 @@ module FriendlyId::NonSluggableInstanceMethods
   end
   
   def update_slugs related_object, friendly_id_was
-    if related_object.class.friendly_id_options[:use_slug]
+    if related_object.class.respond_to?('friendly_id_options') && related_object.class.friendly_id_options[:use_slug]
       Slug.find_all_by_sluggable_type_and_scope(related_object.class.name, friendly_id_was).each {|slug| slug.update_attribute :scope, self.friendly_id}
     end
   end
